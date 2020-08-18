5c977689dda044352343df910a57aeafdb9416b1
jing-s-zhong <59180766+jing-s-zhong@users.noreply.github.com>
2020-08-18 19:58:13 -0400

    Upgrade to use the cron expression

---
+++ scripts/cronScheduleTester.sql
@@ -0,0 +1,222 @@
+USE DATABASE BI;
+USE DATABASE BI_TEST;
+USE SCHEMA _CONTROL_LOGIC;
+
+--------------------------------------------------------
+-- Cron expression item parser
+--------------------------------------------------------
+DROP FUNCTION CRON_ITEM_PARSER(VARCHAR, FLOAT, FLOAT);
+SHOW FUNCTIONS;
+CREATE OR REPLACE FUNCTION CRON_ITEM_PARSER (
+	CRON_ITEM STRING,
+	CRON_FIRST FLOAT,
+	CRON_LAST FLOAT
+)
+  RETURNS VARIANT
+  LANGUAGE JAVASCRIPT
+AS
+$$
+function cronItemParse(cronItem, cronFirst, cronLast) {
+	var itemOptions = [],
+		cronStart = cronFirst, 
+		cronStop = cronLast, 
+		cronStep = 1;
+	if (cronItem.includes("-") || cronItem.includes("/")) {
+		var bounds = cronItem.split("-").map(x => parseInt(x));
+		cronStart = isNaN(bounds[0]) ? cronFirst : bounds[0];
+		if (bounds.length > 1) {
+			cronStop =  isNaN(bounds[1]) ? (isNaN(bounds[0]) ? cronFirst : cronLast) : bounds[1]
+		}
+
+		var repeats = cronItem.split("-")[0].split("/").map(x => parseInt(x));
+		if (repeats.length > 1) {
+			cronStart = repeats[0];
+			cronStep = repeats[1]
+		}
+	}
+	else if (cronItem === "L") {
+		cronStart = cronLast;
+		cronStop = cronLast
+	}
+	else if (cronItem != "*") {
+		cronStart = parseInt(cronItem);
+		cronStop = parseInt(cronItem)
+	}
+    else if (cronLast == 59) {
+        cronStep = 5
+    }
+	for (i = cronStart; i <= cronStop; i+= cronStep) {
+		itemOptions.push(i)
+	}
+    //return [cronStart, cronStop, cronStep];
+    return itemOptions;
+}
+
+return cronItemParse(CRON_ITEM, CRON_FIRST, CRON_LAST);
+$$
+;
+
+SELECT CRON_ITEM_PARSER('0/5-', 0, 59);
+
+--------------------------------------------------------
+-- Cron schedule tester for current timestamp
+--------------------------------------------------------
+DROP FUNCTION CRON_SCHEDULE_TESTER(VARCHAR);
+SHOW FUNCTIONS;
+CREATE OR REPLACE FUNCTION CRON_SCHEDULE_TESTER (
+	CRON_EXPRESSION STRING
+)
+  RETURNS BOOLEAN
+  LANGUAGE JAVASCRIPT
+AS
+$$
+function cronItemParse(cronItem, cronFirst, cronLast) {
+	var itemOptions = [],
+		cronStart = cronFirst, 
+		cronStop = cronLast, 
+		cronStep = 1;
+	if (cronItem.includes("-") || cronItem.includes("/")) {
+		var bounds = cronItem.split("-").map(x => parseInt(x));
+		cronStart = isNaN(bounds[0]) ? cronFirst : bounds[0];
+		if (bounds.length > 1) {
+			cronStop =  isNaN(bounds[1]) ? (isNaN(bounds[0]) ? cronFirst : cronLast) : bounds[1]
+		}
+
+		var repeats = cronItem.split("-")[0].split("/").map(x => parseInt(x));
+		if (repeats.length > 1) {
+			cronStart = repeats[0];
+			cronStep = repeats[1]
+		}
+	}
+	else if (cronItem === "L") {
+		cronStart = cronLast;
+		cronStop = cronLast
+	}
+	else if (cronItem != "*") {
+		cronStart = parseInt(cronItem);
+		cronStop = parseInt(cronItem)
+	}
+    else if (cronLast == 59) {
+        cronStep = 5
+    }
+	for (i = cronStart; i <= cronStop; i+= cronStep) {
+		itemOptions.push(i)
+	}
+    //return [cronStart, cronStop, cronStep];
+    return itemOptions;
+}
+
+function cronScheduleTest(cronExpression) {
+	// parse the current timestamp
+    var testTimestamp = new Date();
+	var testDayOfWeek = testTimestamp.getDay();
+	let [testDate, testTime] = testTimestamp.toISOString().split("T");
+	let [testYear, testMonth, testDayOfMonth] = testDate.split("-").map(x => parseInt(x));
+	let [testHour, testMinute, testSecond] = testTime.slice(0,9).split(":").map(x => parseInt(x));
+	var lastDayOfMonth = (new Date(testYear, testMonth +1, 0)).getDate();
+	
+	// parse the cron expression
+	let [cronMinutes, cronHours, cronDayOfMonth, cronMonth, cronDayOfWeek] = cronExpression.split(" ");
+	
+	// test cron items
+	return cronItemParse(cronMinutes, 0, 59).includes(testMinute)
+		&& cronItemParse(cronHours, 0, 23).includes(testHour)
+		&& cronItemParse(cronDayOfMonth, 1, lastDayOfMonth).includes(testDayOfMonth)
+		&& cronItemParse(cronMonth, 1, 12).includes(testMonth)
+		&& cronItemParse(cronDayOfWeek, 0, 6).includes(testDayOfWeek);		
+}
+
+return cronScheduleTest(CRON_EXPRESSION);
+$$
+;
+
+SELECT CURRENT_TIMESTAMP, CRON_SCHEDULE_TESTER('40 0-15 18 * 2');
+
+
+--------------------------------------------------------
+-- Cron schedule tester for specified timestamp
+--------------------------------------------------------
+DROP FUNCTION CRON_SCHEDULE_TESTER(VARCHAR, TIMESTAMP_NTZ);
+SHOW FUNCTIONS;
+CREATE OR REPLACE FUNCTION CRON_SCHEDULE_TESTER (
+	CRON_EXPRESSION STRING,
+	TEST_TIMESTAMP TIMESTAMP_NTZ
+)
+  RETURNS BOOLEAN
+  LANGUAGE JAVASCRIPT
+AS
+$$
+function cronItemParse(cronItem, cronFirst, cronLast) {
+	var itemOptions = [],
+		cronStart = cronFirst, 
+		cronStop = cronLast, 
+		cronStep = 1;
+	if (cronItem.includes("-") || cronItem.includes("/")) {
+		var bounds = cronItem.split("-").map(x => parseInt(x));
+		cronStart = isNaN(bounds[0]) ? cronFirst : bounds[0];
+		if (bounds.length > 1) {
+			cronStop =  isNaN(bounds[1]) ? (isNaN(bounds[0]) ? cronFirst : cronLast) : bounds[1]
+		}
+
+		var repeats = cronItem.split("-")[0].split("/").map(x => parseInt(x));
+		if (repeats.length > 1) {
+			cronStart = repeats[0];
+			cronStep = repeats[1]
+		}
+	}
+	else if (cronItem === "L") {
+		cronStart = cronLast;
+		cronStop = cronLast
+	}
+	else if (cronItem != "*") {
+		cronStart = parseInt(cronItem);
+		cronStop = parseInt(cronItem)
+	}
+    else if (cronLast == 59) {
+        cronStep = 5
+    }
+	for (i = cronStart; i <= cronStop; i+= cronStep) {
+		itemOptions.push(i)
+	}
+    //return [cronStart, cronStop, cronStep];
+    return itemOptions;
+}
+
+function cronScheduleTest(cronExpression, testTimestamp) {
+	// parse the presented timestamp
+	var testDayOfWeek = testTimestamp.getDay();
+	let [testDate, testTime] = testTimestamp.toISOString().split("T");
+	let [testYear, testMonth, testDayOfMonth] = testDate.split("-").map(x => parseInt(x));
+	let [testHour, testMinute, testSecond] = testTime.slice(0,9).split(":").map(x => parseInt(x));
+	var lastDayOfMonth = (new Date(testYear, testMonth +1, 0)).getDate();
+	
+	// parse the cron expression
+	let [cronMinutes, cronHours, cronDayOfMonth, cronMonth, cronDayOfWeek] = cronExpression.split(" ");
+	
+	// test cron items
+	return cronItemParse(cronMinutes, 0, 59).includes(testMinute)
+		&& cronItemParse(cronHours, 0, 23).includes(testHour)
+		&& cronItemParse(cronDayOfMonth, 1, lastDayOfMonth).includes(testDayOfMonth)
+		&& cronItemParse(cronMonth, 1, 12).includes(testMonth)
+		&& cronItemParse(cronDayOfWeek, 0, 6).includes(testDayOfWeek);		
+}
+
+return cronScheduleTest(CRON_EXPRESSION, TEST_TIMESTAMP);
+$$
+;
+
+SELECT CURRENT_TIMESTAMP, CRON_SCHEDULE_TESTER('* 7-21 18 * *', CURRENT_TIMESTAMP);
+
+
+--------------------------------------------------------
+-- Cron schedule query for specified timestamp
+--------------------------------------------------------
+SELECT * 
+FROM _CONTROL_LOGIC.SINGLE_SNOW_QUERY_JOBS
+WHERE CRON_SCHEDULE_TESTER(SCHEDULE_EXPRESSION, '2020-08-18 15:15:16'::TIMESTAMP_NTZ)
+;
+
+SELECT * 
+FROM _CONTROL_LOGIC.SINGLE_SNOW_QUERY_JOBS
+WHERE CRON_SCHEDULE_TESTER(SCHEDULE_EXPRESSION, '2020-08-18 12:15:16'::TIMESTAMP_NTZ)
+;
--- scripts/schedule1_deploy.sql
+++ scripts/schedule1_deploy.sql
@@ -1,80 +1,89 @@
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- Add new found business elements and manual entries data into the tables
+-- Single snow query job manager
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 USE DATABASE BI;
+USE DATABASE BI_TEST;
 USE SCHEMA _CONTROL_LOGIC;
 -------------------------------------------------------------
--- Setup a finder manager 
+-- Setup a job manager 
 -------------------------------------------------------------
 --
--- Create a finder list table
+-- Create a job management table
 --
-CREATE OR REPLACE TABLE NEW_BUSINESS_ELEMENT_FINDERS (
-  FINDER_LABEL VARCHAR COMMENT 'Unique label of the finder',
-  FINDER_QUERY VARCHAR COMMENT 'SQL query of the finder',
-  FINDER_TYPE VARCHAR COMMENT 'Category of the finder',
-  FINDER_NOTE VARCHAR COMMENT 'Finder change note',
-  FINDER_ENABLED BOOLEAN COMMENT 'Finder status',
-  HOURS_ON_SCHEDULE VARIANT COMMENT 'Finder schedule pattern',        -- 0-23: daily scheduled hour; 24: 24 hours a day
-  TIME_OF_LAST_RUN TIMESTAMP_NTZ COMMENT 'Last run time of the finder',
-  STATUS_OF_LAST_RUN VARCHAR COMMENT 'SLast run status of the finder'
+CREATE OR REPLACE TABLE SINGLE_SNOW_QUERY_JOBS (
+  JOB_LABEL VARCHAR COMMENT 'Unique label of the job',
+  JOB_QUERY VARCHAR COMMENT 'SQL query of the job',
+  JOB_TYPE VARCHAR COMMENT 'Category of the job',
+  JOB_NOTE VARCHAR COMMENT 'Job change note',
+  JOB_ENABLED BOOLEAN COMMENT 'Job status',
+  JOB_PRIORITY NUMBER COMMENT 'Scheudle order of the job',
+  SCHEDULE_BATCH_ID NUMBER COMMENT 'Scheduled in batch by same batch_id',
+  SCHEDULE_EXPRESSION VARIANT COMMENT 'Job schedule pattern',        -- 0-23: daily scheduled hour; 24: 24 hours a day
+  TIME_OF_NEXT_RUN TIMESTAMP_NTZ COMMENT 'Time of the next job schedule',
+  TIME_OF_LAST_RUN TIMESTAMP_NTZ COMMENT 'Time of the last job schedule',
+  STATUS_OF_LAST_RUN VARCHAR COMMENT 'SLast run status of the job'
 );
+--ALTER TABLE SINGLE_SNOW_QUERY_JOBS RENAME COLUMN SCHEDULE_ON_HOURS TO SCHEDULE_EXPRESSION;
+
 --
--- Create a finder scheduler SP
+-- Create a job scheduler SP for whole set
 --
-CREATE OR REPLACE PROCEDURE NEW_BUSINESS_ELEMENT_REFRESH(FINDER_ENABLED BOOLEAN)
+CREATE OR REPLACE PROCEDURE SINGLE_SNOW_QUERY_JOB_SCHEDULER(
+	JOB_ENABLED BOOLEAN
+	)
 RETURNS STRING
 LANGUAGE JAVASCRIPT STRICT
 AS
 $$
 try {
-	var finderLabel = '', finderQuery = '', finderType = '', finderNote = '', finder_Rslt = '';
+	var jobLabel = '', jobQuery = '', jobType = '', jobNote = '', job_rslt = '';
 	var querySet = [], resultSet = [], noteSet = [];
 
 	//-----------------------------------------------
-	// Obtain and loop all finders 
+	// Obtain and loop all jobs 
 	//-----------------------------------------------
 	var snow_sql = `
-        SELECT FINDER_LABEL, FINDER_QUERY, FINDER_TYPE, FINDER_NOTE
-        FROM NEW_BUSINESS_ELEMENT_FINDERS
-        WHERE FINDER_ENABLED = :1
-        AND (ARRAY_CONTAINS(DATE_PART('HH',CURRENT_TIMESTAMP), HOURS_ON_SCHEDULE) 
-             OR ARRAY_CONTAINS(24, HOURS_ON_SCHEDULE)
-             OR FINDER_ENABLED = 0)
+        SELECT JOB_LABEL, JOB_QUERY, JOB_TYPE, JOB_NOTE
+        FROM SINGLE_SNOW_QUERY_JOBS
+        WHERE JOB_ENABLED = :1
+        AND (ARRAY_CONTAINS(DATE_PART('HH',CURRENT_TIMESTAMP), SCHEDULE_EXPRESSION) 
+             OR ARRAY_CONTAINS(24, SCHEDULE_EXPRESSION)
+             OR JOB_ENABLED = 0)
+        ORDER BY JOB_PRIORITY
 		`;
 	var snow_stmt = snowflake.createStatement({
 	  sqlText: snow_sql,
-	  binds: [FINDER_ENABLED]
+	  binds: [JOB_ENABLED]
       }); 
 	var snow_list = snow_stmt.execute(); 
     
 	while (snow_list.next())
 	{
-        finderLabel = snow_list.getColumnValue(1);
-        finderQuery = snow_list.getColumnValue(2);
-        finderType = snow_list.getColumnValue(3);
-        finderNote = snow_list.getColumnValue(4);
+        jobLabel = snow_list.getColumnValue(1);
+        jobQuery = snow_list.getColumnValue(2);
+        jobType = snow_list.getColumnValue(3);
+        jobNote = snow_list.getColumnValue(4);
         try {
-            var finder_stmt = snowflake.createStatement({ sqlText: finderQuery }); 
-            finder_Rslt = finder_stmt.execute(); 
+            var job_Stmt = snowflake.createStatement({ sqlText: jobQuery }); 
+            job_rslt = job_Stmt.execute(); 
         }
         catch (err1) {
-            finder_Rslt = 'Error->' + err1; 
+            job_rslt = 'Error->' + err1; 
         }    
         finally {
-            resultSet.push({"query" : finderQuery, "type": finderType, "note": finderNote, "result": finder_Rslt});
-            var finderStatus = finder_Rslt.toString().startsWith("Error") ? 'FAILURE' : 'SUCCESS';
+            resultSet.push({"query" : jobQuery, "type": jobType, "note": jobNote, "result": job_rslt});
+            var jobStatus = job_rslt.toString().startsWith("Error") ? 'FAILURE' : 'SUCCESS';
             
             var snow_sql = `
-            UPDATE NEW_BUSINESS_ELEMENT_FINDERS 
+            UPDATE SINGLE_SNOW_QUERY_JOBS 
             SET TIME_OF_LAST_RUN = CURRENT_TIMESTAMP,
                 STATUS_OF_LAST_RUN = :2
-            WHERE FINDER_LABEL = :1            
+            WHERE JOB_LABEL = :1            
             `;
             
             var snow_stmt = snowflake.createStatement({
               sqlText: snow_sql,
-              binds: [finderLabel, finderStatus]
+              binds: [jobLabel, jobStatus]
               }); 
             var snow_log = snow_stmt.execute(); 
             
@@ -90,3 +99,147 @@ catch (err) {
 	return "Failed: " + err;
 }
 $$;
+--
+--
+-- Create a job scheduler SP for jobs in a batch
+--
+CREATE OR REPLACE PROCEDURE SINGLE_SNOW_QUERY_JOB_SCHEDULER(
+	JOB_ENABLED BOOLEAN,
+	BATCH_ID FLOAT
+	)
+RETURNS STRING
+LANGUAGE JAVASCRIPT STRICT
+AS
+$$
+function cronItemParse(cronItem, cronFirst, cronLast) {
+	var itemOptions = [],
+		cronStart = cronFirst, 
+		cronStop = cronLast, 
+		cronStep = 1;
+	if (cronItem.includes("-") || cronItem.includes("/")) {
+		var bounds = cronItem.split("-").map(x => parseInt(x));
+		cronStart = isNaN(bounds[0]) ? cronFirst : bounds[0];
+		if (bounds.length > 1) {
+			cronStop =  isNaN(bounds[1]) ? (isNaN(bounds[0]) ? cronFirst : cronLast) : bounds[1]
+		}
+
+		var repeats = cronItem.split("-")[0].split("/").map(x => parseInt(x));
+		if (repeats.length > 1) {
+			cronStart = repeats[0];
+			cronStep = repeats[1]
+		}
+	}
+	else if (cronItem === "L") {
+		cronStart = cronLast;
+		cronStop = cronLast
+	}
+	else if (cronItem != "*") {
+		cronStart = parseInt(cronItem);
+		cronStop = parseInt(cronItem)
+	}
+    else if (cronLast == 59) {
+        cronStep = 5
+    }
+	for (i = cronStart; i <= cronStop; i+= cronStep) {
+		itemOptions.push(i)
+	}
+    //return [cronStart, cronStop, cronStep];
+    return itemOptions;
+}
+
+function cronScheduleTest(cronExpression, testTimestamp) {
+	// parse the presented timestamp
+	var testDayOfWeek = testTimestamp.getDay();
+	let [testDate, testTime] = testTimestamp.toISOString().split("T");
+	let [testYear, testMonth, testDayOfMonth] = testDate.split("-").map(x => parseInt(x));
+	let [testHour, testMinute, testSecond] = testTime.slice(0,9).split(":").map(x => parseInt(x));
+	var lastDayOfMonth = (new Date(testYear, testMonth +1, 0)).getDate();
+	
+	// parse the cron expression
+	let [cronMinutes, cronHours, cronDayOfMonth, cronMonth, cronDayOfWeek] = cronExpression.split(" ");
+	
+	// test cron items
+	return cronItemParse(cronMinutes, 0, 59).includes(testMinute)
+		&& cronItemParse(cronHours, 0, 23).includes(testHour)
+		&& cronItemParse(cronDayOfMonth, 1, lastDayOfMonth).includes(testDayOfMonth)
+		&& cronItemParse(cronMonth, 1, 12).includes(testMonth)
+		&& cronItemParse(cronDayOfWeek, 0, 6).includes(testDayOfWeek);		
+}
+
+try {
+	var jobLabel = '', jobQuery = '', jobType = '', jobNote = '', job_rslt = '';
+	var querySet = [], resultSet = [], noteSet = [];
+	//-----------------------------------------------
+	// Obtain and loop all jobs 
+	//-----------------------------------------------
+	var snow_sql = `
+        SELECT JOB_LABEL, JOB_QUERY, JOB_TYPE, JOB_NOTE, SCHEDULE_EXPRESSION JOB_CRON
+        FROM SINGLE_SNOW_QUERY_JOBS
+        WHERE JOB_ENABLED = :1
+		AND SCHEDULE_BATCH_ID = :2
+        AND COALESCE(DATEDIFF('MINUTE',TIME_OF_NEXT_RUN, CURRENT_TIMESTAMP),0) <= 0
+        ORDER BY JOB_PRIORITY
+		`;
+	var snow_stmt = snowflake.createStatement({
+	  sqlText: snow_sql,
+	  binds: [JOB_ENABLED, BATCH_ID]
+      }); 
+	var snow_list = snow_stmt.execute(); 
+    
+	while (snow_list.next())
+	{
+        jobLabel = snow_list.getColumnValue(1);
+        jobQuery = snow_list.getColumnValue(2);
+        jobType = snow_list.getColumnValue(3);
+        jobNote = snow_list.getColumnValue(4);
+        jobCron = snow_list.getColumnValue(5);
+        try {
+            var job_Stmt = snowflake.createStatement({ sqlText: jobQuery }); 
+            job_rslt = job_Stmt.execute(); 
+        }
+        catch (err1) {
+            job_rslt = 'Error->' + err1; 
+        }    
+        finally {
+            resultSet.push({"query" : jobQuery, "type": jobType, "note": jobNote, "result": job_rslt});
+            var jobStatus = job_rslt.toString().startsWith("Error") ? 'FAILURE' : 'SUCCESS';
+
+			var timeMinuteTag = new Date(); timeMinuteTag.setSeconds(0,0);
+			//var yearSpan = new Date(timeMinuteTag.setFullYear(timeMinuteTag.getFullYear() + 1));
+			//while ((!cronScheduleTest(jobCron, timeMinuteTag)) && timeMinuteTag <= yearSpan)
+			while (!cronScheduleTest(jobCron, timeMinuteTag))
+			{
+			  timeMinuteTag.setMinutes(timeMinuteTag.getMinutes() + 1);
+			}
+           
+            var snow_sql = `
+            UPDATE SINGLE_SNOW_QUERY_JOBS 
+            SET TIME_OF_NEXT_RUN = :4,
+				TIME_OF_LAST_RUN = CURRENT_TIMESTAMP,
+                STATUS_OF_LAST_RUN = :2
+            WHERE JOB_LABEL = :1 
+			AND SCHEDULE_BATCH_ID = :3        
+            `;
+            
+            var snow_stmt = snowflake.createStatement({
+              sqlText: snow_sql,
+              binds: [jobLabel, jobStatus, BATCH_ID, timeMinuteTag.toISOString()]
+              }); 
+            var snow_log = snow_stmt.execute(); 
+            
+        }
+    }
+    	
+	//-----------------------------------------------
+    // Done and return the result-set
+	//-----------------------------------------------
+	return JSON.stringify(resultSet);
+}
+catch (err) {
+	return "Failed: " + err;
+}
+$$;
+
+
+CALL SINGLE_SNOW_QUERY_JOB_SCHEDULER(1, 1);
+select * from SINGLE_SNOW_QUERY_JOBS;
